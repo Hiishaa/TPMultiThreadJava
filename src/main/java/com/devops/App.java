60e88af8dc9e955b3907981a2a194396abdb74f6
hichem <hichem.meneceur@outlook.fr>
2026-01-12 19:39:04 +0100

    Question 5 effectuée

--- src/main/java/com/devops/App.java
+++ src/main/java/com/devops/App.java
@@ -8,9 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        ThreadMatch thread1 = new ThreadMatch("Lyon", "Marseille");
-        ThreadMatch thread2 = new ThreadMatch("Saint-Etienne", "Lyon");
-        ThreadMatch thread3 = new ThreadMatch("Marseille", "Saint-Etienne");
+        ThreadMatch thread1 = new ThreadMatch("Lyon", "Marseille", 3, 0, 0);
+        ThreadMatch thread2 = new ThreadMatch("Saint-Etienne", "Lyon", 3, 0, 0);
+        ThreadMatch thread3 = new ThreadMatch("Marseille", "Saint-Etienne", 3, 0, 0 );
         Thread t1 = new Thread(thread1);
         Thread t2 = new Thread(thread2);
         Thread t3 = new Thread(thread3);
--- src/main/java/com/devops/ThreadMatch.java
+++ src/main/java/com/devops/ThreadMatch.java
@@ -1,28 +1,40 @@
 package com.devops;
 
-public class ThreadMatch implements Runnable{
+public class ThreadMatch implements Runnable {
 
     String equipe1;
     String equipe2;
+    int nbCommentaires;
+    int scoreEquipe1;
+    int scoreEquipe2;
 
-    public ThreadMatch(String equipe1, String equipe2) {
+    public ThreadMatch(String equipe1, String equipe2, int nbCommentaires, int scoreEquipe1, int scoreEquipe2) {
         this.equipe1 = equipe1;
         this.equipe2 = equipe2;
+        this.nbCommentaires = nbCommentaires;
+        this.scoreEquipe1 = scoreEquipe1;
+        this.scoreEquipe2 = scoreEquipe2;
     }
 
     @Override
-    public void run(){
+    public void run() {
+        System.out.println("Début du match : " + this.equipe1 + " - " + this.equipe2);
+        for (int i = 0; i < 3; i++) {
 
-            for(int i = 0; i < 2; i++ )
-            {
-           System.out.println("Début du match : " + this.equipe1 + " - " + this.equipe2);
-           String comment = CommentGenerator.getRandomComment();
-           System.out.println(comment);
-           try{
-           Thread.sleep((int)(Math.random()*10000));
-           } catch (InterruptedException e) {
-               e.printStackTrace();
-           }
+            String comment = CommentGenerator.getRandomComment();
+            System.out.println(this.equipe1 + " - " + this.equipe2 + " : " + comment);
+            if (comment.contains("But des locaux")) {
+                scoreEquipe1++;
+            } else if (comment.contains("But des visiteurs")) {
+                scoreEquipe2++;
+            }
         }
+        try {
+            Thread.sleep((int) (Math.random() * 1000));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Fin du match entre "+this.equipe1 + " et " +this.equipe2 +"|"+"Score final : "+this.scoreEquipe1 + " - " +this.scoreEquipe2);
     }
+
 }
